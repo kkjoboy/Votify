1af489c7da41fdb1fc98ab4636897f726ba1ff6f
ashlyno <ashlyno@uw.edu>
2017-04-06 09:38:10 -0700

    updated web

--- WebDev/order.js
+++ WebDev/order.js
@@ -5,16 +5,34 @@ angular.module('VoteApp', ['ngSanitize', 'ui.router', 'ui.bootstrap']) //ngSanit
 .config(function($stateProvider, $urlRouterProvider) {
 
 	$stateProvider.state('home', {
-			url: '/', 
+			url: '/home', 
 			templateUrl: 'partials/home.html',
 			controller: 'HomeCtrl'
 		})	
-		$urlRouterProvider.otherwise('/'); //other route
-
+	.state('newsfeed', {
+		url: '/newsfeed',
+		templateUrl: 'partials/newsfeed.html',
+		controller: 'NewsCtrl'
+	})
+	.state('article', {
+		url: '/articles',
+		templateUrl: 'partials/articles.html',
+		controller: 'ArtCtrl'
+	})
+	$urlRouterProvider.otherwise('/'); //other route
 })
 
 //home page settings
 .controller('HomeCtrl', ['$scope', '$http', function($scope, $http) {
+
+
+}])
+
+.controller('NewsCtrl', ['$scope', '$http', function($scope, $http) {
+
+
+}])
+.controller('ArtCtrl', ['$scope', '$http', function($scope, $http) {
 
 
 }]);
---
+++ WebDev/partials/articles.html
@@ -0,0 +1,3 @@
+<div>
+    <p>Heloo</p>
+</div>
